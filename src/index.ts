cecb3a875510ee413ca9ce2e847ffacd520e6b9f
Aleksandr <laptop@MacBook-Air-Aleksandr.local>
2025-09-05 16:15:09 +0300

    added button price

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,13 @@ import "./styles/main.scss";
 import { initCategoriesSlider } from "./ts/categoriesSlider";
 import { initProductsSlider } from "./ts/productsSlider";
 
-// импорт баннера (Webpack сам подставит путь в dist)
+
 import salesBanner from "./assets/images/unsplash.jpg";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // инициализация слайдера
   initCategoriesSlider("#categoriesSlider");
   initProductsSlider("#productsSlider");
 
-    // установка фона для блока .container-sales
     const salesSection = document.querySelector<HTMLElement>(".container-sales");
 
     if (salesSection) {
